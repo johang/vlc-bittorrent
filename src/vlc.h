f4f811f6324af06d4cffbd26a6a72a5521a39752
Johan Gunnarsson <johan.gunnarsson@gmail.com>
2016-12-12 22:07:58 +0100

    Remove some dead code

--- src/vlc.h
+++ src/vlc.h
@@ -1,38 +1,10 @@
 #ifndef VLC_BITTORRENT_VLC_H
 #define VLC_BITTORRENT_VLC_H
 
+// Workaround because VLC's vlc_atomic.h is borked for VLC 2.2.x.
 #ifndef VLC_ATOMIC_H
 #define VLC_ATOMIC_H
-
-// Workaround because VLC's vlc_atomic.h is borked for VLC 2.2.x.
-
 #include <atomic>
-
-#if 0
-typedef std::atomic_uint_least32_t vlc_atomic_float;
-
-static inline void vlc_atomic_init_float(vlc_atomic_float *var, float f)
-{
-    union { float f; uint32_t i; } u;
-    u.f = f;
-    atomic_init(var, u.i);
-}
-
-static inline float vlc_atomic_load_float(vlc_atomic_float *atom)
-{
-    union { float f; uint32_t i; } u;
-    u.i = atomic_load(atom);
-    return u.f;
-}
-
-static inline void vlc_atomic_store_float(vlc_atomic_float *atom, float f)
-{
-    union { float f; uint32_t i; } u;
-    u.f = f;
-    atomic_store(atom, u.i);
-}
-#endif
-
 #endif
 
 #pragma GCC diagnostic push
