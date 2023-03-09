3b501321234a0a06b0695cdf25794d3c07ad85b5
おり <kok.in@yahoo.com>
2023-03-09 18:16:30 +0300

    Added test build, but need more checks.

--- .cmake/config_checks.cmake
+++ .cmake/config_checks.cmake
@@ -1,5 +1,9 @@
 include (CheckIncludeFile)
 
+if (CMake_CXX14_WORKS)
+    set (HAVE_CXX14 1)
+endif (CMake_CXX14_WORKS)
+
 check_include_file ("dlfcn.h"       HAVE_DLFNC_H)
 check_include_file ("inttypes.h"    HAVE_INTTYPES_H)
 check_include_file ("stdint.h"      HAVE_STDINT_H)
@@ -7,6 +11,6 @@ check_include_file ("stdio.h"       HAVE_STDIO_H)
 check_include_file ("stdlib.h"      HAVE_STDLIB_H)
 check_include_file ("strings.h"     HAVE_STRINGS_H)
 check_include_file ("string.h"      HAVE_STRING_H)
-check_include_file ("sys/stats.h"   HAVE_SYS_STAT_H)
+check_include_file ("sys/stat.h"    HAVE_SYS_STAT_H)
 check_include_file ("sys/types.h"   HAVE_SYS_TYPES_H)
 check_include_file ("unistd.h"      HAVE_UNISTD_H)
